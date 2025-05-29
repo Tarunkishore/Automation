9c060f6d3ee27b474dbacbc42027393fe4e34540
tarunkishore <tarunkishore@192.168.29.135>
2025-05-29 21:15:11 +0530

    ItestListener Removed

--- src/main/java/Stepsdefinitions/Common.java
+++ src/main/java/Stepsdefinitions/Common.java
@@ -39,7 +39,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 
-public class Common implements ITestListener {
+public class Common {
 	public static WebDriver driver;
 	public static ExtentSparkReporter sparkReporter;	// UI of the report
 	public static ExtentReports extentReports;		// populate common info of the report
